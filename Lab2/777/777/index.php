88cc10ba988ba58ac660531b08eebd546a05942c
Diego Quezada <dquezada-munoz@csumb.edu>
2018-02-05 22:58:27 +0000

    Lab2

---
+++ Lab2/777/777/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <meta charset="utf-8"/>
+        <style>
+            footer
+            {
+                text-align: center;
+                clear: both;
+                font-size:.8em;
+                padding-top: 10px;
+            }
+            footer img
+            {
+                clear: both;
+                display: block;
+                margin: 0px auto;
+                
+            }
+        </style>
+    </head>
+    <body>
+        
+        <h1>
+            HERE
+        </h1>
+        
+        <img src="777/img/bar.png" alt="bar">
+        
+        
+        
+        
+        
+        <footer>
+            <br/><br/>
+            <div id="ft">
+                <hr>
+                Internet Programming. 2018&copy; Quezada <br/>
+                <strong>Disclaimer:</strong> The information in this webpage is fictitious. <br/>
+                It is used for academic purposes only.
+            </div>
+            <br/>
+            <img src="777/img/csumb_logo_150_86.jpg" alt"csumbLogo">
+        </footer>
+    </body>
+</html>
