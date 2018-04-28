8b20ef9697c59c2173bfee4aa92728c38d7d2668
Diego Quezada <dquezada-munoz@csumb.edu>
2018-04-28 22:42:25 +0000

    Lab9

--- Lab9/checkUsernameAPI.php
+++ Lab9/checkUsernameAPI.php
@@ -6,8 +6,8 @@
     
     $username = $_GET['username'];
     
-    //next query allows for SQL injection because of the single quotes
-    //$sql = "SELECT * FROM lab9_user WHERE username = '$username'";
+    // next query allows for SQL injection because of the single quotes
+    // $sql = "SELECT * FROM lab9_user WHERE username = '$username'";
     
     $sql = "SELECT * FROM lab9_user WHERE username = :username";
     
@@ -16,7 +16,7 @@
     $stmt->execute( array(":username"=>$username));
     $record = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    //print_r($record);
+    // print_r($record);
     
     echo json_encode($record);
 
